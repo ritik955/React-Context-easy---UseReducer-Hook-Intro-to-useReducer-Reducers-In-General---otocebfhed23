4ccb5be83de9cde2167a7f2c9aa1213653021bdc
Ritik Gupta <61592876+ritik955@users.noreply.github.com>
2022-10-02 13:05:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import '../styles/App.css';
 import { UserProfile } from './UserProfile';
+import { createContext } from 'react';
 
-
+const UserContext = createContext();
 const App = () => {
 
+const user = {name:"Newton", age:3}
   return (
     <div id="main">
-      
-        <UserProfile />
-      
+      <UserContext.Provider value={user}>
+      <UserProfile />
+      </UserContext.Provider>
     </div>
   )
 }
